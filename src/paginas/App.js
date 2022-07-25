176dcf4af6231319f2d4ea55f80e642ff60b8686
vmaffioli <vinimaffioli@gmail.com>
2022-07-24 23:05:36 -0300

    ajustes grid inicial

--- src/paginas/App.js
+++ src/paginas/App.js
@@ -1,18 +1,36 @@
-import Cabecalho from '../componentes/cabecalho';
-import Selecionaveis from '../componentes/selecionaveis';
-import Janela from '../componentes/janela';
+import React from 'react';
+
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+
+
+
 
 
 
 function App() {
+
+
+
+
   return (
-    <>
 
-      <Cabecalho />
-      <Selecionaveis />
-      <Janela />
+    <DndProvider backend={HTML5Backend}>
 
-    </>
+
+      <div class="container">
+        <div class="cabecalho"></div>
+        <div class="corpo">
+          <div class="janela"></div>
+          <div class="selecionaveis"></div>
+        </div>
+      </div>
+
+
+
+    </DndProvider>
+
   );
 }
 
